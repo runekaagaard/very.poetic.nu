29de0634e8694d336951975306d00f8194aef7f0
runekaagaard <rumi.kg@gmail.com>
2011-09-25 21:39:31 +0200

    Site is working. Needs row based permissions, show login/signup links and to make disqus more simple looking.

--- very_poetic_nu/poems/views.py
+++ very_poetic_nu/poems/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import get_object_or_404, render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from very_poetic_nu.poems.models import Poem
 from itertools import groupby
+from django.core.exceptions import ObjectDoesNotExist
 
 def index(request):
 	poems = Poem.objects.select_related().order_by('author__id').all()
 	return render_to_response("poems/templates/index.html", {
 		'poems': Poem.objects.select_related().order_by('author__id').all()})
 
-def poem(request, id):
-	poem = get_object_or_404(Poem, pk=id)
-	return render_to_response("poems/templates/poem.html", {'poem': poem})
+def poem(request, pk):
+	pk = int(pk)
+	poem = get_object_or_404(Poem, pk=pk)
+	try:
+		poem_to_left = Poem.objects.get(pk=pk-1) 
+	except ObjectDoesNotExist:
+		poem_to_left=None
+	try:
+		poem_to_right = Poem.objects.get(pk=pk+1)
+	except ObjectDoesNotExist:
+		poem_to_right=None
+	
+	return render_to_response("poems/templates/poem.html", {
+		'poem': poem,
+		'poem_to_left': poem_to_left,
+		'poem_to_right': poem_to_right,
+	})
+
+def random(request):
+	return HttpResponseRedirect(Poem.objects.order_by('?')[0].id)
--- very_poetic_nu/urls.py
+++ very_poetic_nu/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'very_poetic_nu.poems.views.index', name='index'),
     url(r'^([0-9]+)$', 'very_poetic_nu.poems.views.poem', name='poem'),
+    url(r'^random$', 'very_poetic_nu.poems.views.random', name='random'),
     # url(r'^very_poetic_nu/', include('very_poetic_nu.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
